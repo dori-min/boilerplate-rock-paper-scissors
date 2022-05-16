4f2630b41a5980cd9fccd7535a574d312f11294a
dori-min <970504542@qq.com>
2022-05-16 16:07:46 +0800

    trial

--- RPS.py
+++ RPS.py
@@ -2,9 +2,14 @@
 
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
+    guess = "R" # initialization
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-#test
+    # 1. Counting R/P/S most used by opponent
+    # opponent_history = ["P","P","P","S","S","R"]
+    countdict = {"R":opponent_history.count("R"),"P":opponent_history.count("P"),"S":opponent_history.count("S")}
+    countdict = dict(sorted(countdict.items(),key=lambda x: x[1],reverse=True))
+    guess = [key for key in countdict.keys()][0]
+
+    # if len(opponent_history) > 2:
+    #     guess = opponent_history[-2]
     return guess
